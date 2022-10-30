125bac66a8ce9055cb61506eed2bcca6abaa67bf
iamsammysamuels <iamsammysamuels@gmail.com>
2022-10-30 15:11:31 +0100

    AirBnB clone - The console

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,18 +2,20 @@
 """Uittests for user module"""
 
 
+from models.user import User
 from models.base_model import BaseModel
 import pep8
 import unittest
 from datetime import datetime
 import uuid
 
+
 class TestUser(unittest.TestCase):
     """Tests the user class"""
 
     def setUp(self):
         """ """
-        self.user_ = user()
+        self.user_ = User()
 
     def tearDown(self):
         """ """
@@ -21,6 +23,7 @@ class TestUser(unittest.TestCase):
 
     def test_pep8_connformation_for_user(self):
         """tests that user module is pep8styled"""
+
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(["models/user"])
         pep8_result = result.total_errors
@@ -34,4 +37,38 @@ class TestUser(unittest.TestCase):
         pep8_result = result.total_errors
         self.assertEqual(pep8_result, 0, "Found code style errors (and warnings).")  # noqa
 
-    def test_
+    def test_type(self):
+        """type checks for user model
+        """
+        self.assertEqual(issubclass(self.user_.__class__, User), True)
+        self.assertEqual(isinstance(self.user_, User), True)
+        self.assertEqual(isinstance(self.user_, User), True)
+        self.assertEqual(type(self.user_), User)
+
+    def test_basic_attribute_set(self):
+        """Basic attribute tests for user model
+        """
+        self.user_.first_name = 'Meco'
+        self.user_.last_name = 'Montes'
+        self.assertEqual(self.user_.first_name, 'Meco')
+        self.assertEqual(self.user_.last_name, 'Montes')
+
+    def test_email(self):
+        """tests the user's email attribute
+        """
+        self.assertEqual(type(User.email), str)
+
+    def test_password(self):
+        """tests the user's password attribute
+        """
+        self.assertEqual(type(User.password), str)
+
+    def test_first_name(self):
+        """tests the user's first_name attribute
+        """
+        self.assertEqual(type(User.first_name), str)
+
+    def test_last_name(self):
+        """tests the user's last_name attribute
+        """
+        self.assertEqual(type(User.last_name), str)
